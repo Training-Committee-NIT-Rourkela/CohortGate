c7c9b47b97c77a14848efc36070c5ac8caea1d79
Shahab-16 <mdshahabuddin0516@gmail.com>
2025-03-18 02:00:17 +0530

    setup-tailwindCSS and added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <>Training Committee - NIT, Rourkela</>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* Header or Main Content */}
+      <div className="flex-grow text-center">
+        <p className="text-4xl font-bold py-8">
+          Training Committee - NIT, Rourkela
+        </p>
+      </div>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
