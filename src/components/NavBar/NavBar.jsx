2b2d3afb3ef1efdaf8b072d34b689d5650617005
Spandan-Mishra <spandanmishra69@gmail.com>
2025-03-19 00:13:26 +0530

    Completed Navbar with transitions

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,19 @@
-import { AlignJustify } from "lucide-react";
+import { AlignJustify as Hamburger } from "lucide-react";
 import React, { useState } from "react";
 
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const links = [
+    { name: "Home", url: "#" },
+    { name: "About Us", url: "#" },
+    { name: "Form", url: "#" },
+    { name: "Contact", url: "#" },
+  ]
 
   return (
     <>
       <div className="flex flex-col items-center justify-center h-full w-full mt-2">
-        <nav className="fixed top-0 left-1/2 w-11/12 -translate-x-1/2 z-50 bg-amber-200 shadow-md py-2 mt-4 px-4 rounded-full">
+        <nav className="fixed top-0 left-1/2 w-11/12 -translate-x-1/2 z-50 bg-white shadow-md py-2 mt-4 px-4 rounded-full border-b border-black">
           <div className="max-w-screen-xl flex items-center justify-between w-full p-2">
 
             {/* NITR Logo */}
@@ -16,35 +22,35 @@ function NavBar() {
             </div>
 
             {/* Desktop Nav Links */}
-            <div className="hidden md:flex items-center space-x-4">
-              <a href="#" className="text-gray-900">Home</a>
-              <a href="#" className="text-gray-900">About</a>
-              <a href="#" className="text-gray-900">Contact</a>
+            <div className="hidden md:flex items-center space-x-6">
+              {links.map((link, index) => (
+                <a key={index} href={link.url} className="relative text-gray-900 hover:scale-110 duration-300 group">
+                  {link.name}
+                  <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-gray-900 transition-all duration-300 group-hover:w-full"></span>
+                </a>
+              ))}
             </div>
 
             {/* Hamburger */}
             <div className="relative md:hidden">
-              <AlignJustify onClick={() => setIsOpen(!isOpen)} />
+              <Hamburger onClick={() => setIsOpen(!isOpen)} />
             </div>
 
           </div>
 
           {/* Mobile Nav Links */}
-          {isOpen && (
-            <div className="absolute top-22 left-0 w-full md:hidden">
-              <ul className="flex flex-col items-center space-y-4 bg-amber-100 py-4 px-4 rounded-lg">
-                <li>
-                  <a href="#" className="text-gray-900">Home</a>
+          <div className={`absolute top-22 left-0 w-full md:hidden transition-all duration-300 ease-in-out transform ${isOpen ? "translate-y-0 opacity-100" : "-translate-y-4 opacity-0 pointer-events-none"}`}>
+            <ul className="flex flex-col items-center space-y-4 bg-white shadow-md border-b border-black py-4 px-4 rounded-lg">
+              {links.map((link, index) => (
+                <li key={index}>
+                  <a href={link.url} className="relative text-gray-900 hover:scale-110 duration-300 group">
+                    {link.name}
+                    <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-gray-900 transition-all duration-300 group-hover:w-full"></span>
+                  </a>
                 </li>
-                <li>
-                  <a href="#" className="text-gray-900">About</a>
-                </li>
-                <li>
-                  <a href="#" className="text-gray-900">Contact</a>
-                </li>
-              </ul>
-            </div>
-          )}
+              ))}
+            </ul>
+          </div>
 
         </nav>
       </div>
