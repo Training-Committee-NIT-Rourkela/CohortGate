2ccc1bb83ba3be74577443254373dbe43f0255b1
chayanc7mondal <mitalichayan18082004@gmail.com>
2025-03-19 19:31:23 +0530

    Added Footer component with social links and Back to Top button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <>Training Committee - NIT, Rourkela</>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow">Training Committee - NIT, Rourkela</main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
